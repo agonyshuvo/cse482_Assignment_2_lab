5720d702693f90e6270c7a5b8b94ef384a54d199
Shuvo Khan <agonysuvo@gmail.com>
2022-07-24 16:46:31 +0600

    24 july
    Code does not work ... check method post and  sql

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 $server = "localhost";
-$username = "root";
-$password = "";
+$username = "mr.UseR";
+$password = "Sk575357##";
 $database = "cse-482";
 
 $conn = mysqli_connect($server, $username, $password, $database);
